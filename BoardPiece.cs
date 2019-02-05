973efc360d03ee15627ea2437d2bf4588d5831f1
RasmusAgergaard <rasmusap@gmail.com>
2019-02-05 16:29:13 +0100

    0.0.2.0

---
+++ BoardPiece.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VikingChess
+{
+    class BoardPiece
+    {
+        public Vector2 position { get; set; }
+
+        //Constructor
+        public BoardPiece()
+        {
+            position = new Vector2(50, 50);
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -1,81 +1,95 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace VikingChess
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
+        //Graphics
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        //Sprites
+        Texture2D spritePiece;
+
+        //Pieces
+        private BoardPiece[,] board;
+        private List<BoardPiece> boardPieces;
+
+        //Piece
+        private BoardPiece piece1;
+        private BoardPiece piece2;
+        private BoardPiece piece3;
+
+        //Constructor
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 500;
+            graphics.PreferredBackBufferHeight = 500;
+            //Window.IsBorderless = true;
+            //graphics.IsFullScreen = true;
+            graphics.ApplyChanges();
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            //Initialize
+            board = new BoardPiece[11, 11];
+            piece1 = new BoardPiece();
+            piece2 = new BoardPiece();
+            piece3 = new BoardPiece();
+
+            //Add pieces to board
+            board[0, 0] = piece1;
+            board[2, 2] = piece2;
+            board[4, 4] = piece3;
+
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            spritePiece = Content.Load<Texture2D>("piece");
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            //Exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
+                
 
-            // TODO: Add your update logic here
+            //Update
 
+            
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+
+            //Draw
+
 
             base.Draw(gameTime);
         }
