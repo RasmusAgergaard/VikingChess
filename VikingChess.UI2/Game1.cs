981cc670ce89a2122f51006b696c32ab1ff5aeff
RasmusAgergaard <rasmusap@gmail.com>
2019-04-08 11:59:28 +0200

    Created a new UI project

---
+++ VikingChess.UI2/Game1.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using VikingChessBL;
+
+namespace VikingChess.UI2
+{
+    public class Game1 : Game
+    {
+        //Init
+        GameSetup gameSetup = new GameSetup();
+        PlayBoard board;
+        CollisionHandler collisionHandler;
+        GameplayHandler gameplayHandler;
+
+        //Graphics
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        //Sprites
+        Texture2D spriteTileGrass;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 500;
+            graphics.PreferredBackBufferHeight = 500;
+            graphics.ApplyChanges();
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            board = new PlayBoard(11, 11, new Vector2(0));
+            board.SetRules(doesAttackersHaveKing: false,
+                           doesDefendersHaveKing: true,
+                           doesAttackerKingWantsToFlee: false,
+                           doesDefenderKingWantsToFlee: true);
+
+            gameplayHandler = new GameplayHandler(board);
+            collisionHandler = new CollisionHandler();
+
+            this.IsMouseVisible = true;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            spriteTileGrass = Content.Load<Texture2D>(@"Tiles\tile_grass");
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            //spriteBatch.Begin();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            var windowWidth = GraphicsDevice.Viewport.Width;
+            var windowHeight = GraphicsDevice.Viewport.Height;
+            var squareWidth = 128;
+            var squareHeight = 64;
+
+
+            var tileX = windowWidth / 2;
+            var tileY = windowHeight / 2;
+            var drawRect = new Rectangle(tileX, tileY, 128, 128);
+
+            spriteBatch.Draw(spriteTileGrass, drawRect, Color.White);
+
+            base.Draw(gameTime);
+
+            spriteBatch.End();
+        }
+    }
+}
