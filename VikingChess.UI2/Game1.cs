559adb2c5ecff1ba0ecbdf1c89cbcf63aa2aa485
RasmusAgergaard <rasmusap@gmail.com>
2019-04-12 14:24:57 +0200

    UI2 now show tiles, refuges, mouse hover and pieces...

--- VikingChess.UI2/Game1.cs
+++ VikingChess.UI2/Game1.cs
@@ -25,10 +25,10 @@ namespace VikingChess.UI2
         Texture2D spritePieceDefenderNormal;
         Texture2D spritePieceDefenderKing;
         Texture2D spritePieceAttackerNormal;
-        Texture2D spriteRefuge;
+        Texture2D spriteRefugeCorner;
+        Texture2D spriteRefugeCenter;
         Texture2D spriteSelectedPiece;
         Texture2D spriteLegalMove;
-        Texture2D spriteTestSquare;
 
         //Fonts
         SpriteFont normalFont;
@@ -67,10 +67,10 @@ namespace VikingChess.UI2
             spritePieceDefenderNormal = Content.Load<Texture2D>(@"Pieces\piece_defender_normal");
             spritePieceDefenderKing = Content.Load<Texture2D>(@"Pieces\piece_defender_king");
             spritePieceAttackerNormal = Content.Load<Texture2D>(@"Pieces\piece_attacker_normal");
-            spriteRefuge = Content.Load<Texture2D>(@"Refuges\tile_refuge");
+            spriteRefugeCorner = Content.Load<Texture2D>(@"Refuges\tile_refuge_corner");
+            spriteRefugeCenter = Content.Load<Texture2D>(@"Refuges\tile_refuge_center");
             spriteSelectedPiece = Content.Load<Texture2D>(@"Layers\selected_piece");
             spriteLegalMove = Content.Load<Texture2D>(@"Layers\legal_move");
-            spriteTestSquare = Content.Load<Texture2D>(@"Test\square");
 
             //Fonts
             normalFont = Content.Load<SpriteFont>(@"Fonts\normal");
@@ -152,42 +152,22 @@ namespace VikingChess.UI2
 
         private void DrawGameArea()
         {
-
-            DrawTiles();
-            DrawMouseHover();
-            DrawPieces();
-        }
-
-        private void DrawMouseHover()
-        {
-            var mouseState = Mouse.GetState();
-            var mousePoint = new Point(mouseState.X, mouseState.Y);
-
-            var spriteWidth = 64;
-            var spriteHeight = 64;
-            var colissionBoxwidth = 32;
-            var colissionBoxHeight = 18;
-
             for (int x = 0; x < board.Columns; x++)
             {
                 for (int y = 0; y < board.Rows; y++)
                 {
-                    var posX = (int)board.BoardPositions[x, y].X + 16;
-                    var posY = (int)board.BoardPositions[x, y].Y + 23;
-                    var drawPosX = (int)board.BoardPositions[x, y].X;
-                    var drawPosY = (int)board.BoardPositions[x, y].Y;
-
-                    if (collisionHandler.PointColisionWithBox(mousePoint.X, mousePoint.Y, posX, posY, colissionBoxwidth, colissionBoxHeight))
-                    {
-                        //Hover
-                        var drawRect = new Rectangle(drawPosX, drawPosY, spriteWidth, spriteHeight);
-                        spriteBatch.Draw(spriteSelectedPiece, drawRect, Color.White);
-                    }
+                    DrawTiles(x, y);
+                    DrawMouseHover(x, y);
+                    DrawPieces(x, y);
                 }
             }
+
+
+
         }
 
-        private void DrawTiles()
+       
+        private void DrawTiles(int x, int y)
         {
             var spriteWidth = 40;
             var spriteHeight = 40;
@@ -196,77 +176,106 @@ namespace VikingChess.UI2
             var drawStartX = (windowWidth / 2) - (boardWidth / 2);
             var drawStartY = (windowHeight / 2) - (boardHeight / 2);
 
-            for (int x = 0; x < board.Columns; x++)
+            var drawX = (x * spriteWidth) + drawStartX;
+            var drawY = (y * spriteHeight) + drawStartY;
+            var drawRect = new Rectangle(drawX, drawY, spriteWidth, spriteHeight);
+
+            //Grass tile
+            spriteBatch.Draw(spriteTileGrass, drawRect, Color.White);
+
+            //Refuge
+            if (board.Board[x, y] != null && board.Board[x, y].Team == Piece.teams.refuge)
             {
-                for (int y = 0; y < board.Rows; y++)
+                //Center
+                if (board.Board[x, y].Type == Piece.types.centerRefuge)
                 {
-                    var drawX = (x * spriteWidth) + drawStartX;
-                    var drawY = (y * spriteHeight) + drawStartY;
-                    var drawRect = new Rectangle(drawX, drawY, spriteWidth, spriteHeight);
-
-                    //Grass tile
-                    spriteBatch.Draw(spriteTileGrass, drawRect, Color.White);
-
-                    //Refuge
-                    if (board.Board[x, y] != null && board.Board[x, y].Team == Piece.teams.refuge)
-                    {
-                        spriteBatch.Draw(spriteRefuge, drawRect, Color.White);
-                    }
-
-                    //Legal moves
-                    if (gameplayHandler.SelectedPiece != null)
-                    {
-                        if (board.LegalMoves[x, y] != null)
-                        {
-                            spriteBatch.Draw(spriteLegalMove, drawRect, Color.White);
-                        }
-                    }
+                    spriteBatch.Draw(spriteRefugeCenter, drawRect, Color.White);
                 }
+
+                //Center
+                if (board.Board[x, y].Type == Piece.types.cornerRefuge)
+                {
+                    spriteBatch.Draw(spriteRefugeCorner, drawRect, Color.White);
+                }
+            }
+
+            //Legal moves
+            if (gameplayHandler.SelectedPiece != null)
+            {
+                if (board.LegalMoves[x, y] != null)
+                {
+                    spriteBatch.Draw(spriteLegalMove, drawRect, Color.White);
+                }
+
             }
         }
 
-        private void DrawPieces()
+        private void DrawMouseHover(int x, int y)
         {
+            var mouseState = Mouse.GetState();
+            var mousePoint = new Point(mouseState.X, mouseState.Y);
+
+            var spriteWidth = 40;
+            var spriteHeight = 40;
+            var boardWidth = spriteWidth * board.Columns;
+            var boardHeight = spriteHeight * board.Rows;
+            var drawStartX = (windowWidth / 2) - (boardWidth / 2);
+            var drawStartY = (windowHeight / 2) - (boardHeight / 2);
+
+            var drawX = (x * spriteWidth) + drawStartX;
+            var drawY = (y * spriteHeight) + drawStartY;
+
+            if (collisionHandler.PointColisionWithBox(mousePoint.X, mousePoint.Y, drawX, drawY, spriteWidth, spriteHeight))
+            {
+                //Hover
+                var drawRect = new Rectangle(drawX, drawY, spriteWidth, spriteHeight);
+                spriteBatch.Draw(spriteSelectedPiece, drawRect, Color.White);
+
+            }
+        }
+
+        private void DrawPieces(int x, int y)
+        {
+            var spriteWidth = 40;
+            var spriteHeight = 40;
+            var boardWidth = spriteWidth * board.Columns;
+            var boardHeight = spriteHeight * board.Rows;
+            var drawStartX = (windowWidth / 2) - (boardWidth / 2);
+            var drawStartY = (windowHeight / 2) - (boardHeight / 2);
             var pieceWidth = 24;
             var pieceHeight = 44;
-            var pieceOffsetX = 20;
-            var pieceOffsetY = -6;
+            var pieceOffsetX = 8;
+            var pieceOffsetY = -16;
 
-            for (int x = 0; x < board.Columns; x++)
+            if (board.Board[x, y] != null)
             {
-                for (int y = 0; y < board.Rows; y++)
+                var drawX = (x * spriteWidth) + drawStartX + pieceOffsetX;
+                var drawY = (y * spriteHeight) + drawStartY + pieceOffsetY;
+                var drawRect = new Rectangle(drawX, drawY, pieceWidth, pieceHeight);
+
+                //Defenders
+                if (board.Board[x, y].Team == Piece.teams.defenders)
                 {
-                    if (board.Board[x, y] != null)
+                    //Normal
+                    if (board.Board[x, y].Type == Piece.types.normalPiece)
                     {
-                        var drawX = (int)board.BoardPositions[x, y].X + pieceOffsetX;
-                        var drawY = (int)board.BoardPositions[x, y].Y + pieceOffsetY;
-                        var drawRect = new Rectangle(drawX, drawY, pieceWidth, pieceHeight);
+                        spriteBatch.Draw(spritePieceDefenderNormal, drawRect, Color.White);
+                    }
 
-                        //Defenders
-                        if (board.Board[x, y].Team == Piece.teams.defenders)
-                        {
-                            //Normal
-                            if (board.Board[x, y].Type == Piece.types.normalPiece)
-                            {
-                                spriteBatch.Draw(spritePieceDefenderNormal, drawRect, Color.White);
-                            }
+                    //King
+                    if (board.Board[x, y].Type == Piece.types.kingPiece)
+                    {
+                        spriteBatch.Draw(spritePieceDefenderKing, drawRect, Color.White);
+                    }
+                }
 
-                            //King
-                            if (board.Board[x, y].Type == Piece.types.kingPiece)
-                            {
-                                spriteBatch.Draw(spritePieceDefenderKing, drawRect, Color.White);
-                            }
-                        }
-
-                        //Attackers
-                        if (board.Board[x, y].Team == Piece.teams.attackers)
-                        {
-                            //Normal
-                            if (board.Board[x, y].Type == Piece.types.normalPiece)
-                            {
-                                spriteBatch.Draw(spritePieceAttackerNormal, drawRect, Color.White);
-                            }
-                        }
+                //Attackers
+                if (board.Board[x, y].Team == Piece.teams.attackers)
+                {
+                    //Normal
+                    if (board.Board[x, y].Type == Piece.types.normalPiece)
+                    {
+                        spriteBatch.Draw(spritePieceAttackerNormal, drawRect, Color.White);
                     }
                 }
             }
